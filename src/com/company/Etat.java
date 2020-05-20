8b31555119ad85aacf248b22ef9c7fe1cff3448e
Ramdane Mouloua <ramdane.mouloua@u-psud.fr>
2020-05-20 21:27:04 +0200

    Update Enum Etat

--- src/com/company/Etat.java
+++ src/com/company/Etat.java
@@ -1,6 +1,39 @@
 package com.company;
+import java.util.Random;
 
 public enum Etat {
-    none, normale, inondee, submergee
+    none{
+        @Override
+        public String toString() {
+            return "none";
+        }
+    },
+
+    normale{
+        @Override
+        public String toString() {
+            return "normale";
+        }
+    }, inondee{
+        @Override
+        public String toString() {
+            return "inondee";
+        }
+    }, submergee{
+        @Override
+        public String toString() {
+            return "submergee";
+        }
+    };
+
+    public static Etat randomEtat()  {
+        return Etat.values()[new Random().nextInt(Etat.values().length)];
+    }
+
+    public static void main(String[] args) {
+        Etat a = Etat.inondee;
+        System.out.print(a.toString());
+    }
+
 }
 
