5ecf37aa4dabb8433848f2ad039183e82893ff14
Guildart <romainmussard974@gmail.com>
2020-05-21 15:44:40 +0200

    Creation Classe Key

---
+++ src/com/company/Key.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public enum Key {
+    air(0), eau(0), feu(0), vent(0);
+
+    private int Qte;
+
+    Key(int Qte){
+        this.Qte = Qte;
+    }
+
+    public int getQuantity(){
+        return this.Qte;
+    }
+
+    public void setQuantity(){
+        this.Qte+=1;
+    }
+}
